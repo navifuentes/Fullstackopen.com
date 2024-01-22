0d9091ed4138640e44568a746483d2166f66d8cc
Ivan Fuentes <iv.ifa94@gmail.com>
2024-01-22 15:32:35 -0300

    part 2.3

--- part2/courseinfo/src/components/Content.jsx
+++ part2/courseinfo/src/components/Content.jsx
@@ -3,11 +3,17 @@ import Part from "./Part";
 
 const Content = ({ course }) => {
   const { parts } = course;
+
+  const total = parts.reduce((s, part) => {
+    return s + part.exercises;
+  }, 0);
+
   return (
     <>
       {parts.map((x) => (
         <Part key={x.id} name={x.name} exercises={x.exercises} />
       ))}
+      <strong>total of {total} exercises</strong>
     </>
   );
 };
