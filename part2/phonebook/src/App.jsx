64b05eb41977bc2c2747431d72e23b4faa886ba7
Ivan Fuentes <iv.ifa94@gmail.com>
2024-02-01 19:51:50 -0300

    part 2.6

---
+++ part2/phonebook/src/App.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import Person from "./components/Person";
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
+
+  const addPerson = (e) => {
+    e.preventDefault();
+    const personObject = {
+      name: newName,
+    };
+    setPersons(persons.concat(personObject));
+    setNewName("");
+  };
+
+  const handleChange = (e) => {
+    setNewName(e.target.value);
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input onChange={handleChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map((x) => (
+        <Person name={x.name} />
+      ))}
+    </div>
+  );
+};
+
+export default App;
