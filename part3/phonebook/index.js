060d74e884fc76562be9ef29e3da3751b4c9ab2b
Ivan Fuentes <iv.ifa94@gmail.com>
2024-02-09 20:35:10 -0300

    part 3.3

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -33,6 +33,14 @@ app.get("/", (req, res) => {
 app.get("/api/persons", (req, res) => {
   res.json(persons);
 });
+app.get("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const person = persons.find((p) => p.id === id);
+  if (person) {
+    res.json(person);
+  }
+  res.status(404).end();
+});
 app.get("/info", (req, res) => {
   res.send(`
   <p>Phonebook has info for ${persons.length} people<p>
