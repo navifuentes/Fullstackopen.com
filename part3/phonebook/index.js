a41ab242d092c890ed75fa6b8a036e9c9e6af0b3
Ivan Fuentes <iv.ifa94@gmail.com>
2024-02-09 20:31:52 -0300

    part 3.2

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -26,11 +26,18 @@ let persons = [
   },
 ];
 
+//GET
 app.get("/", (req, res) => {
   res.send("<h1>Hello World!</h1>");
 });
 app.get("/api/persons", (req, res) => {
   res.json(persons);
+});
+app.get("/info", (req, res) => {
+  res.send(`
+  <p>Phonebook has info for ${persons.length} people<p>
+  <p>${new Date()}</p>
+  `);
 });
 
 const PORT = 3001;
