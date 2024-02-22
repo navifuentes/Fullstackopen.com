2efda526c00814acb8244bb96424c4c8a6cef578
Ivan Fuentes <iv.ifa94@gmail.com>
2024-02-22 22:53:03 +0100

    part 4.4

--- part4/blogs-app/utils/list_helper.js
+++ part4/blogs-app/utils/list_helper.js
@@ -2,6 +2,10 @@ const dummy = (blogs) => {
   return 1;
 };
 
+const totalLikes = (blogs) =>
+  blogs.map((blog) => blog.likes).reduce((acc, curr) => acc + curr, 0);
+
 module.exports = {
   dummy,
+  totalLikes,
 };
