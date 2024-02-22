c36dda7e6c2cbc9aff87d23514e54c77b5a4256a
Ivan Fuentes <iv.ifa94@gmail.com>
2024-02-22 23:20:53 +0100

    part 4.5

--- part4/blogs-app/utils/list_helper.js
+++ part4/blogs-app/utils/list_helper.js
@@ -5,7 +5,15 @@ const dummy = (blogs) => {
 const totalLikes = (blogs) =>
   blogs.map((blog) => blog.likes).reduce((acc, curr) => acc + curr, 0);
 
+const favoriteBlog = (blogs) => {
+  const maxLikesNumber = Math.max(...blogs.map((blog) => blog.likes));
+  const result = blogs.find((blog) => blog.likes === maxLikesNumber);
+  const { title, author, likes } = result;
+  return { title, author, likes };
+};
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 };
