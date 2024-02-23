bc3630f22742b71ab0fdf41e1dded7565847ad8a
Ivan Fuentes <iv.ifa94@gmail.com>
2024-02-23 21:11:16 +0100

    part 4.6 4.7

--- part4/blogs-app/utils/list_helper.js
+++ part4/blogs-app/utils/list_helper.js
@@ -12,8 +12,34 @@ const favoriteBlog = (blogs) => {
   return { title, author, likes };
 };
 
+const mostBlogs = (blogs) => {
+  const authorsBlogs = blogs.reduce((authors, blog) => {
+    const authorExist = authors.findIndex((x) => x.author === blog.author);
+    authorExist !== -1
+      ? authors[authorExist].blogs++
+      : authors.push({ author: blog.author, blogs: 1 });
+
+    return authors;
+  }, []);
+  return authorsBlogs.reduce((a, b) => (a.blogs > b.blogs ? a : b));
+};
+
+const mostLikes = (blogs) => {
+  const authorLikes = blogs.reduce((authors, blog) => {
+    const authorExist = authors.findIndex((x) => x.author === blog.author);
+    authorExist !== -1
+      ? (authors[authorExist].likes += blog.likes)
+      : authors.push({ author: blog.author, likes: blog.likes });
+
+    return authors;
+  }, []);
+  return authorLikes.reduce((a, b) => (a.likes > b.likes ? a : b));
+};
+
 module.exports = {
   dummy,
   totalLikes,
   favoriteBlog,
+  mostBlogs,
+  mostLikes,
 };
