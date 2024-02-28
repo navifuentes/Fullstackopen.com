f1352ad51de7349d2fa515f8187aba61824456d5
Ivan Fuentes <iv.ifa94@gmail.com>
2024-02-28 09:23:43 +0100

    part 5.5 to 5.7

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,25 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-);
+import { useEffect, useState } from "react";
+import Togglable from "./Togglable";
+
+const Blog = ({ blog }) => {
+  const [blogLikes, setBlogLikes] = useState(0);
+
+  useEffect(() => {
+    setBlogLikes(blog.likes);
+  }, []);
+
+  return (
+    <div className="blog">
+      {blog.title} {blog.author}
+      <Togglable>
+        {blog.url}
+        <br />
+        {blog.likes} <button>Like</button>
+        <br />
+        {blog.user.name}
+      </Togglable>
+    </div>
+  );
+};
 
 export default Blog;
