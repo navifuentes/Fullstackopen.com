88c9f753a6bec3da492dfad0e2f82cfcf1f8ea80
Ivan Fuentes <iv.ifa94@gmail.com>
2024-03-02 20:12:42 +0100

    part 6.9

--- part6/redux-anecdotes/src/AnecdoteList.jsx
+++ part6/redux-anecdotes/src/AnecdoteList.jsx
@@ -3,13 +3,17 @@ import { useSelector, useDispatch } from "react-redux";
 import { voteAnecdote } from "./reducers/anecdoteReducer";
 
 const AnecdoteList = () => {
-  const anecdotes = useSelector((state) =>
-    state.sort((a, b) => b.votes - a.votes)
-  );
+  const anecdotesList = useSelector(({ anecdotes, filter }) => {
+    const sorted = anecdotes.sort((a, b) => b.votes - a.votes);
+
+    return filter === ""
+      ? sorted
+      : sorted.filter((a) => a.content.includes(filter));
+  });
   const dispatch = useDispatch();
   return (
     <>
-      {anecdotes.map((anecdote) => (
+      {anecdotesList.map((anecdote) => (
         <div key={anecdote.id}>
           <div>{anecdote.content}</div>
           <div>
--- part6/redux-anecdotes/src/App.jsx
+++ part6/redux-anecdotes/src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import AnecdoteForm from "./AnecdoteForm";
 import AnecdoteList from "./AnecdoteList";
+import Filter from "./Filter";
 
 const App = () => {
   return (
     <div>
       <h2>Anecdotes</h2>
+      <Filter />
       <AnecdoteList />
       <AnecdoteForm />
     </div>
